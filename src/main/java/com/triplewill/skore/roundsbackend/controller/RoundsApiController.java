79bbd12d5a5067d83332afc825cc3cf253e6d6a3
triplewill <erikw.englund@gmail.com>
2021-12-29 11:36:25 +0100

    Add OpenAPI/SwaggerUI docs

--- src/main/java/com/triplewill/skore/roundsbackend/controller/RoundsApiController.java
+++ src/main/java/com/triplewill/skore/roundsbackend/controller/RoundsApiController.java
@@ -1,15 +1,40 @@
 package com.triplewill.skore.roundsbackend.controller;
 
 import com.triplewill.skore.roundsbackend.domain.entity.Round;
+import com.triplewill.skore.roundsbackend.service.RoundsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.UUID;
+
 @RestController
+@RequestMapping("/api/rounds")
+@RequiredArgsConstructor
 public class RoundsApiController {
 
-    @GetMapping("/rounds")
-    public Round getHello(){
-        return new Round();
+    private final RoundsService roundsService;
+
+    @GetMapping
+    public Collection<Round> getRounds(){
+        System.out.println("GET /api/rounds");
+        return roundsService.getRounds();
+    }
+
+    @GetMapping("/{id}")
+    public Round getRoundById(@PathVariable UUID id){
+        System.out.println("GET /api/rounds/" + id);
+        return roundsService.getRoundById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Round updateRound(@PathVariable UUID id){
+        System.out.println("PUT /api/rounds/" + id);
+        return roundsService.updateRound(id);
     }
 
 }
