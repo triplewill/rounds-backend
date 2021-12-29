f0acb4923336f366219722ec59b94e06c3dfa7ea
triplewill <erikw.englund@gmail.com>
2021-12-29 11:46:44 +0100

    Add parameter descriptions in controller

--- src/main/java/com/triplewill/skore/roundsbackend/controller/RoundsApiController.java
+++ src/main/java/com/triplewill/skore/roundsbackend/controller/RoundsApiController.java
@@ -3,6 +3,7 @@ package com.triplewill.skore.roundsbackend.controller;
 import com.triplewill.skore.roundsbackend.domain.entity.Round;
 import com.triplewill.skore.roundsbackend.service.RoundsService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,14 +30,14 @@ public class RoundsApiController {
 
     @Operation(summary = "Get a round by its id")
     @GetMapping("/{id}")
-    public Round getRoundById(@PathVariable UUID id){
+    public Round getRoundById(@Parameter(description = "id of round to be fetched") @PathVariable UUID id){
         System.out.println("GET /api/rounds/" + id);
         return roundsService.getRoundById(id);
     }
 
     @Operation(summary = "Update a specific round")
     @PutMapping("/{id}")
-    public Round updateRound(@PathVariable UUID id){
+    public Round updateRound(@Parameter(description = "id of round to be updated") @PathVariable UUID id){
         System.out.println("PUT /api/rounds/" + id);
         return roundsService.updateRound(id);
     }
