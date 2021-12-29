eff387fec201b922ecff908c80e1dcde774ef855
triplewill <erikw.englund@gmail.com>
2021-12-29 11:44:15 +0100

    Add operation descriptions in controller

--- src/main/java/com/triplewill/skore/roundsbackend/controller/RoundsApiController.java
+++ src/main/java/com/triplewill/skore/roundsbackend/controller/RoundsApiController.java
@@ -2,6 +2,7 @@ package com.triplewill.skore.roundsbackend.controller;
 
 import com.triplewill.skore.roundsbackend.domain.entity.Round;
 import com.triplewill.skore.roundsbackend.service.RoundsService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,18 +20,21 @@ public class RoundsApiController {
 
     private final RoundsService roundsService;
 
+    @Operation(summary = "Get all registered rounds")
     @GetMapping
     public Collection<Round> getRounds(){
         System.out.println("GET /api/rounds");
         return roundsService.getRounds();
     }
 
+    @Operation(summary = "Get a round by its id")
     @GetMapping("/{id}")
     public Round getRoundById(@PathVariable UUID id){
         System.out.println("GET /api/rounds/" + id);
         return roundsService.getRoundById(id);
     }
 
+    @Operation(summary = "Update a specific round")
     @PutMapping("/{id}")
     public Round updateRound(@PathVariable UUID id){
         System.out.println("PUT /api/rounds/" + id);
